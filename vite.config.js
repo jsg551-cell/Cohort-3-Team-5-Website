6b92c91f111a46f6aa8d8cfc7de9190b6b24e378
jsg551-cell <jsg551@york.ac.uk>
2026-01-07 00:35:47 +0000

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base: "Cohort-3-Team-5-Website",
-})
+  plugins: [react(), tailwindcss()],
+  base: "/Cohort-3-Team-5-Website/",
+});
